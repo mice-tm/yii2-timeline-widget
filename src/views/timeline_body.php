3208c820f9a82973658c97bd8bff011e69dd0a0a
mice-tm <mice@templatemonster.me>
2021-12-09 09:46:14 +0200

    Added check for not string values.

--- src/views/timeline_body.php
+++ src/views/timeline_body.php
@@ -7,10 +7,20 @@ TimelineAsset::register($this);
 ?>
 <div class="timeline-header">
     <?php foreach ($items as $key => $values): ?>
+        <?php
+        $old = $values['old'] ?? '';
+        if (!is_string($old)) {
+            $old = json_encode($old);
+        }
+        $new = $values['new'] ?? '';
+        if (!is_string($new)) {
+            $new = json_encode($new);
+        }
+        ?>
         <div class="diff-row">
             <div class="name"><span class="diff-label">Changed: </span><?php echo $key; ?></div>
-            <div class="from"><span class="diff-label">From: </span><?php echo $values['old'] ?? ''; ?></div>
-            <div class="to"><span class="diff-label">To: </span><?php echo $values['new'] ?? ''; ?></div>
+            <div class="from"><span class="diff-label">From: </span><?php echo $old; ?></div>
+            <div class="to"><span class="diff-label">To: </span><?php echo $new ?></div>
         </div>
     <?php endforeach; ?>
 </div>
